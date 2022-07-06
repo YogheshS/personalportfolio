ff117b718e546274c91e3fe1f647ef931643175d
Yogesh <yoghesh07@gmail.com>
2022-07-06 08:41:35 +0530

    Message

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          React Portfolio        </a>
       </header>
     </div>
   );
